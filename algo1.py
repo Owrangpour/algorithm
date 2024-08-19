8d90b58556ffec61e095544ef656ebbd0550938f
Owrangpour <149054882+Owrangpour@users.noreply.github.com>
2024-08-19 17:05:20 +0330

    Add files via upload

---
+++ algo1.py
@@ -0,0 +1,6 @@
+a = int(input(print("Enter the first number:")))
+b = int(input(print("Enter the second number:")))
+c = int(input(print("Enter the third number:")))
+sum = a + b + c
+average = sum/3
+print("average:" , average )
